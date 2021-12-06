83ca0986914486725b2a48b6257998b0e52d07dd
metekog <metekog@gmail.com>
2021-12-07 00:53:56 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Topbar from "./components/topbar/Topbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Write from "./pages/write/Write";
+import Single from "./pages/single/Single";
+import Settings from "./pages/settings/Settings";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
+  const { user } = useContext(Context);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/register">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route exact path="/login">
+          {user ? <Home /> : <Login />}
+        </Route>
+        <Route exact path="/write">
+          {user ? <Write /> : <Register />}
+        </Route>
+        <Route exact path="/settings">
+          {user ? <Settings /> : <Register />}
+        </Route>
+        <Route exact path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
